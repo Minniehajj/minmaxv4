905eb73a5ccde30489f580093863706267cfea90
Minhajul Hoq <minhoq@gmail.com>
2024-02-24 16:20:01 -0500

    chore: more algolia fixing

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -2,29 +2,29 @@
 import algoliasearch from "algoliasearch/lite";
 import { SearchBox } from "react-instantsearch";
 import { InstantSearchNext } from "react-instantsearch-nextjs";
-import Image from "next/image";
+import AlgoliaSVG from "@/public/algolia.svg";
 const searchClient = algoliasearch(
   process.env.NEXT_PUBLIC_ALGOLIA_APP_ID as string,
-  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY as string
+  process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY as string,
+  {}
 );
 
-const client = algoliasearch("latency", "6be0576ff61c053d5f9a3225e2a90f76");
 export const Search = () => {
-  console.log(
-    process.env.NEXT_PUBLIC_ALGOLIA_APP_ID,
-    process.env.NEXT_PUBLIC_ALGOLIA_SEARCH_API_KEY
-  );
   return (
     <>
       <InstantSearchNext
         indexName="articles"
         searchClient={searchClient}
         routing
+        future={{
+          preserveSharedStateOnUnmount: true,
+        }}
       >
         <SearchBox className="text-theme-black" />
       </InstantSearchNext>
-      Powered by{" "}
-      <Image src="algolia.svg" alt="Algolia" width={100} height={20} />
+      <div className="flex gap-4">
+        Powered by <AlgoliaSVG className="w-4 h-4" />
+      </div>
     </>
   );
 };
