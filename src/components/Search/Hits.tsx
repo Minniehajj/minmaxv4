fb29047a911cbc24ac29a0f8c419d8c6fd40dabd
Minhajul Hoq <minhoq@gmail.com>
2024-03-25 20:50:23 -0400

    chore: more hits cleanup

--- src/components/Search/Hits.tsx
+++ src/components/Search/Hits.tsx
@@ -2,17 +2,30 @@
 import Link from "next/link";
 import { useHits } from "react-instantsearch";
 import { ReactNode } from "react";
+import { CardToolTip } from "../CardToolTip";
+import reactStringReplace from "react-string-replace";
+
+const filterText = (text: string) => {
+  const parsedtext = text.replace(/[\u2018\u2019]/g, "'");
+  const p = /\[([\s\w\d-+_/,'’&À-ÿ]*)\]/g;
+
+  const parsedBody = reactStringReplace(parsedtext, p, (match, i) => (
+    <CardToolTip key={`index-${i}`} name={match} />
+  ));
+  return parsedBody;
+};
 export const Hits = () => {
   const { hits } = useHits();
+
   return (
-    <div className="flex max-w-96 flex-col items-center m-auto text-center">
+    <div className="flex max-w-96 flex-col items-center m-auto text-center gap-4">
       {hits.map((hit) => (
-        <article key={hit.objectID} className="relative mb-4">
+        <article key={hit.objectID}>
           <Link href={hit.url as string} className="group">
             <h2 className="mt-2 text-center text-2xl font-bold">
               {hit.title as ReactNode}
             </h2>
-            <p>{hit.content as ReactNode}</p>
+            <p>{filterText(hit.content as string)}</p>
           </Link>
         </article>
       ))}
