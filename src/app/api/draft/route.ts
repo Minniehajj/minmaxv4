9d5020d9fb6902aec924f16acfa8aceb71255285
Minhajul Hoq <minhoq@gmail.com>
2024-04-04 10:08:56 -0400

    chore: added secret

--- src/app/api/draft/route.ts
+++ src/app/api/draft/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const secret = searchParams.get("secret");
   const slug = searchParams.get("slug") as string;
-
+  console.log("incoming secret:", secret);
   if (secret !== process.env.CONTENTFUL_PREVIEW_SECRET) {
     return new Response("Invalid token", { status: 401 });
   }
