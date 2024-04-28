e2c8f1a5492d2983f510d8444a04927a83837844
Minhajul Hoq <minhoq@gmail.com>
2024-04-28 18:39:45 -0400

    chore: fix author route

--- src/app/author/[slug]/page.tsx
+++ src/app/author/[slug]/page.tsx
@@ -10,7 +10,7 @@ const AuthorPosts = async ({ params }: { params: { slug: string } }) => {
   const { isEnabled } = draftMode();
   const posts = await getAllPostsByAuthor(params.slug, 1, isEnabled);
   const { totalPages } = await getAllPostSlugsByAuthor(params.slug, isEnabled);
-
+  console.log("posts", posts);
   return (
     <main className="pt-2 pb-12">
       <PaginatedClient posts={posts} totalPages={totalPages} params={params} />
--- src/lib/fetch/getPosts.ts
+++ src/lib/fetch/getPosts.ts
@@ -90,6 +90,7 @@ export async function getAllPostsByAuthor(
                 }
                 slug
                 title
+                publishDate
               }
             }
           }
