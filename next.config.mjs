9c657d474db796a8dc10d3881fc055ee7ebd6799
Minhajul Hoq <minhoq@gmail.com>
2024-10-29 11:37:42 -0400

    chore: removed headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
-const cspHeader = `
-    default-src 'self';
-    script-src 'self' 'unsafe-eval' 'unsafe-inline';
-    style-src 'self' 'unsafe-inline';
-    img-src 'self' blob: data:;
-    font-src 'self';
-    object-src 'none';
-    base-uri 'self';
-    form-action 'self';
-    frame-ancestors 'https://app.contentful.com';
-    upgrade-insecure-requests;
-    X-Frame-Options 'SAMEORIGIN';
-`
 
 const nextConfig = {
   images: {
@@ -41,19 +28,6 @@ const nextConfig = {
       use: ["@svgr/webpack"],
     });
     return config;
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: cspHeader.replace(/\n/g, ''),
-          },
-        ],
-      },
-    ]
   },
 };
 
