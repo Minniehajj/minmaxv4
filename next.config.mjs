3e33ebf92846686e948dd5255f73c1453d9484f6
Minhajul Hoq <minhoq@gmail.com>
2024-10-29 10:23:02 -0400

    chore: added csp headers

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+const cspHeader = `
+    default-src 'self';
+    script-src 'self' 'unsafe-eval' 'unsafe-inline';
+    style-src 'self' 'unsafe-inline';
+    img-src 'self' blob: data:;
+    font-src 'self';
+    object-src 'none';
+    base-uri 'self';
+    form-action 'self';
+    frame-ancestors 'none';
+    upgrade-insecure-requests;
+`
+
 const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
@@ -27,6 +40,19 @@ const nextConfig = {
       use: ["@svgr/webpack"],
     });
     return config;
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: cspHeader.replace(/\n/g, ''),
+          },
+        ],
+      },
+    ]
   },
 };
 
